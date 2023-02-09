e893e6a7dfb86d6d7edc279cce1a9ab1ced19dc4
Amirali Motahari <Amiralimotahari.dev@outlook.com>
2023-02-09 19:02:53 +0330

    v0.0.10

--- js/main.js
+++ js/main.js
@@ -1,131 +1,156 @@
 //menu button
 let flag = 0;
- let menuButton = document.querySelector("div.menuButton");
+let menuButton = document.querySelector("div.menuButton");
 
-function menuButtonActivator(){
-        document.getElementById("midLine").style.left = "100%";
-        document.getElementById("midLine").style.opacity = "0";
+function menuButtonActivator() {
+  document.getElementById("midLine").style.left = "100%";
+  document.getElementById("midLine").style.opacity = "0";
 
-        document.getElementById("topLine").style.rotate = "45deg";
-        document.getElementById("topLine").style.top = "50%";
-        document.getElementById("topLine").style.translate = "0,-50%";
+  document.getElementById("topLine").style.rotate = "45deg";
+  document.getElementById("topLine").style.top = "50%";
+  document.getElementById("topLine").style.translate = "0,-50%";
 
-        document.getElementById("botLine").style.rotate = "-45deg";
-        document.getElementById("botLine").style.top = "50%";
-        document.getElementById("botLine").style.translate = "0,-50%";
+  document.getElementById("botLine").style.rotate = "-45deg";
+  document.getElementById("botLine").style.top = "50%";
+  document.getElementById("botLine").style.translate = "0,-50%";
 
-        document.querySelector("div.menuPage").style.display = "block";
+  document.querySelector("div.menuPage").style.display = "block";
 
-        // elOne.style.background= "#e5e5e526";
-        // elOne.addEventListener("mouseleave", function () {
-        //     this.style.background = "";
-        //   });
-        // document.querySelector("div.besHov>div.page").style.display = "block";
+  // elOne.style.background= "#e5e5e526";
+  // elOne.addEventListener("mouseleave", function () {
+  //     this.style.background = "";
+  //   });
+  // document.querySelector("div.besHov>div.page").style.display = "block";
 
-        flag = 1;
-       }
-function menuButtonDeactivator(){
-            document.getElementById("midLine").style.left = "";
-            document.getElementById("midLine").style.opacity = "1";
+  flag = 1;
+}
+function menuButtonDeactivator() {
+  document.getElementById("midLine").style.left = "";
+  document.getElementById("midLine").style.opacity = "1";
 
-            document.getElementById("topLine").style.rotate = "";
-            document.getElementById("topLine").style.top = "";
-            document.getElementById("topLine").style.translate = "";
+  document.getElementById("topLine").style.rotate = "";
+  document.getElementById("topLine").style.top = "";
+  document.getElementById("topLine").style.translate = "";
 
-            document.getElementById("botLine").style.rotate = "";
-            document.getElementById("botLine").style.top = "";
-            document.getElementById("botLine").style.translate = "";
+  document.getElementById("botLine").style.rotate = "";
+  document.getElementById("botLine").style.top = "";
+  document.getElementById("botLine").style.translate = "";
 
-            document.querySelector("div.menuPage").style.display = "";
+  document.querySelector("div.menuPage").style.display = "";
 
-            flag = 0;
-        }
+  flag = 0;
+}
 
-menuButton.addEventListener("click",function(){
-    if(flag === 0){
-        menuButtonActivator();
-        ourStoryPageDeactivator();
-        joinUsPageDeactivator();
-        logInPageDeactivator();
-    }
-    else{
-        menuButtonDeactivator();
-    }
+menuButton.addEventListener("click", function () {
+  if (flag === 0) {
+    menuButtonActivator();
+    ourStoryPageDeactivator();
+    joinUsPageDeactivator();
+    logInPageDeactivator();
+  } else {
+    menuButtonDeactivator();
+  }
 });
 
 //log in button
-document.querySelector("div.left>div.logIn").addEventListener("mouseenter",function(){
-    document.querySelector("div.left>div.logIn>button>span").classList.remove("fa-light");
-    document.querySelector("div.left>div.logIn>button>span").classList.add("fa-solid");
-});
-document.querySelector("div.left>div.logIn").addEventListener("mouseleave",function(){
-    document.querySelector("div.left>div.logIn>button>span").classList.remove("fa-solid");
-    document.querySelector("div.left>div.logIn>button>span").classList.add("fa-light");
-});
+document
+  .querySelector("div.left>div.logIn")
+  .addEventListener("mouseenter", function () {
+    document
+      .querySelector("div.left>div.logIn>button>span")
+      .classList.remove("fa-light");
+    document
+      .querySelector("div.left>div.logIn>button>span")
+      .classList.add("fa-solid");
+  });
+document
+  .querySelector("div.left>div.logIn")
+  .addEventListener("mouseleave", function () {
+    document
+      .querySelector("div.left>div.logIn>button>span")
+      .classList.remove("fa-solid");
+    document
+      .querySelector("div.left>div.logIn>button>span")
+      .classList.add("fa-light");
+  });
 
-//search box 
-document.querySelector("div.searchBox>input").addEventListener("focus",function(){
-    document.querySelector("div.searchBox>span").style.visibility="visible";
-    document.querySelector("div.searchBox").style.boxShadow="0 2px 3px rgba(0,0,0,0.16)";
-});
-document.querySelector("div.searchBox>span").addEventListener("click",function(){
-    document.querySelector("div.searchBox>span").style.visibility="hidden";
-    document.querySelector("div.searchBox").style.boxShadow="";
-});
-document.addEventListener("click",function(e){
-    const outCheck = document.querySelector("div.searchBox");
+//search box
+document
+  .querySelector("div.searchBox>input")
+  .addEventListener("focus", function () {
+    document.querySelector("div.searchBox>span").style.visibility = "visible";
+    document.querySelector("div.searchBox").style.boxShadow =
+      "0 2px 3px rgba(0,0,0,0.16)";
+  });
+document
+  .querySelector("div.searchBox>span")
+  .addEventListener("click", function () {
+    document.querySelector("div.searchBox>span").style.visibility = "hidden";
+    document.querySelector("div.searchBox").style.boxShadow = "";
+  });
+document.addEventListener("click", function (e) {
+  const outCheck = document.querySelector("div.searchBox");
 
-    if (!outCheck.contains(e.target)) {
-        document.querySelector("div.searchBox>span").style.visibility="hidden";
-        document.querySelector("div.searchBox").style.boxShadow="";
-    }
+  if (!outCheck.contains(e.target)) {
+    document.querySelector("div.searchBox>span").style.visibility = "hidden";
+    document.querySelector("div.searchBox").style.boxShadow = "";
+  }
 });
-
 
 //shopping bag
 let shoppingBag = document.querySelector("div.right>div.shoppingButton");
 let shoppingbPage = document.querySelector("div.shoppingBagPage");
 
-shoppingBag.addEventListener("mouseenter",function(){
-    document.querySelector("div.right>div.shoppingButton>a>span").classList.remove("fa-light");
-    document.querySelector("div.right>div.shoppingButton>a>span").classList.add("fa-solid");
+shoppingBag.addEventListener("mouseenter", function () {
+  document
+    .querySelector("div.right>div.shoppingButton>a>span")
+    .classList.remove("fa-light");
+  document
+    .querySelector("div.right>div.shoppingButton>a>span")
+    .classList.add("fa-solid");
 
-    shoppingbPage.classList.add("activator");
-    document.querySelector("div.shoppingBagPage>div.shoppingContainer").style.opacity="1";
-    menuButtonDeactivator();
-    ourStoryPageDeactivator();
-    joinUsPageDeactivator();
-    logInPageDeactivator();
+  shoppingbPage.classList.add("activator");
+  document.querySelector(
+    "div.shoppingBagPage>div.shoppingContainer"
+  ).style.opacity = "1";
+  menuButtonDeactivator();
+  ourStoryPageDeactivator();
+  joinUsPageDeactivator();
+  logInPageDeactivator();
+});
+shoppingBag.addEventListener("mouseleave", function () {
+  document
+    .querySelector("div.right>div.shoppingButton>a>span")
+    .classList.remove("fa-solid");
+  document
+    .querySelector("div.right>div.shoppingButton>a>span")
+    .classList.add("fa-light");
+});
+shoppingBag.addEventListener("click", function () {
+  document.querySelector("div.shoppingBagPage").classList.add("activator");
+});
 
-});
-shoppingBag.addEventListener("mouseleave",function(){
-    document.querySelector("div.right>div.shoppingButton>a>span").classList.remove("fa-solid");
-    document.querySelector("div.right>div.shoppingButton>a>span").classList.add("fa-light");
-});
-shoppingBag.addEventListener("click",function(){
-    document.querySelector("div.shoppingBagPage").classList.add("activator");
-});
-
-document.querySelector("div.shoppingBagPage>div.shoppingContainer").addEventListener("mouseleave",function(){
+document
+  .querySelector("div.shoppingBagPage>div.shoppingContainer")
+  .addEventListener("mouseleave", function () {
     this.opacity = "1";
     shoppingbPage.style.display = "none";
     shoppingbPage.classList.remove("activator");
-});
-
+  });
 
 //menu page
 let activator = document.querySelectorAll("div.menuContainer>div.content");
 let target = document.querySelector("div.menuContainer>div.content>div.page");
-activator.forEach((elem)=>{
-    elem.addEventListener("mouseenter",function(){
-        deactivator();
-        elem.childNodes[3].classList.add("activator");
-    })
-})
-function deactivator(){
-    activator.forEach((elem)=>{
-        elem.childNodes[3].classList.remove("activator");
-    })
+activator.forEach((elem) => {
+  elem.addEventListener("mouseenter", function () {
+    deactivator();
+    elem.childNodes[3].classList.add("activator");
+  });
+});
+function deactivator() {
+  activator.forEach((elem) => {
+    elem.childNodes[3].classList.remove("activator");
+  });
 }
 
 //ourstory page
@@ -139,36 +164,33 @@ function ourStoryPageDeactivator() {
   ourStoryPage.classList.remove("activator");
 }
 
-ourStoryButton.addEventListener("click",function(){
-    ourStoryPage.classList.toggle("activator");
-    menuButtonDeactivator();
-    joinUsPageDeactivator();
-    logInPageDeactivator();
+ourStoryButton.addEventListener("click", function () {
+  ourStoryPage.classList.toggle("activator");
+  menuButtonDeactivator();
+  joinUsPageDeactivator();
+  logInPageDeactivator();
 });
 // document.querySelector("div.ourStoryPage>div.ourStoryContainer").addEventListener("mouseleave",function(){
 //     ourStoryPageDeactivator();
 // });
-
 
 //join us page
 let joinUsButton = document.getElementById("joinUs");
 let joinUsPage = document.querySelector("div.joinUsPage");
 
 function joinUsPageActivator() {
-    joinUsPage.classList.add("activator");
+  joinUsPage.classList.add("activator");
 }
 function joinUsPageDeactivator() {
-    joinUsPage.classList.remove("activator");
+  joinUsPage.classList.remove("activator");
 }
 
 joinUsButton.addEventListener("click", function () {
-    joinUsPage.classList.toggle("activator");
-    menuButtonDeactivator();
-    ourStoryPageDeactivator();
-    logInPageDeactivator();
+  joinUsPage.classList.toggle("activator");
+  menuButtonDeactivator();
+  ourStoryPageDeactivator();
+  logInPageDeactivator();
 });
-
-
 
 //login
 let logInButton = document.getElementById("logIn");
@@ -182,8 +204,8 @@ function logInPageDeactivator() {
 }
 
 logInButton.addEventListener("click", function () {
-    logInPage.classList.toggle("activator");
-    menuButtonDeactivator();
-    ourStoryPageDeactivator();
-    joinUsPageDeactivator;
+  logInPage.classList.toggle("activator");
+  menuButtonDeactivator();
+  ourStoryPageDeactivator();
+  joinUsPageDeactivator();
 });
